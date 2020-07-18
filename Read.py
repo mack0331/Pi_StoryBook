febf49be209c964ccd7ccadb8fa1cc8f0670d6fd
Eric MacKay <ericjmackay@gmail.com>
2020-07-17 21:57:23 -0500

    Read.py

---
+++ Read.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import pygame
+
+reader = SimpleMFRC522()
+
+try:
+        id, text = reader.read()
+       # print(id)
+        print("Read File: " + text)
+finally:
+        GPIO.cleanup()
+
+	
+        pygame.mixer.init()
+        pygame.mixer.music.load(text.strip())
+        pygame.mixer.music.play()
+        print("Playing AudioBook: " + text)
+        while pygame.mixer.music.get_busy() == True:
+                continue
