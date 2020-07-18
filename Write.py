7c15d4e4feae197b6a156eee1552e6426bd3049d
mack0331 <ericjmackay@gmail.com>
2020-07-17 22:00:34 -0500

    Update Write.py

--- Write.py
+++ Write.py
@@ -2,21 +2,13 @@
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-import pygame
 
 reader = SimpleMFRC522()
 
 try:
-        id, text = reader.read()
-       # print(id)
-        print("Read File: " + text)
+        text = input('New data:')
+        print("Now place your tag to write")
+        reader.write(text)
+        print("Written")
 finally:
         GPIO.cleanup()
-
-        
-        pygame.mixer.init()
-        pygame.mixer.music.load(text.strip())
-        pygame.mixer.music.play()
-        print("Playing AudioBook: " + text)
-        while pygame.mixer.music.get_busy() == True:
-                continue
