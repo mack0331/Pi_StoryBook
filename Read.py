fb1b4b5dece08d5ad6b139a7bdc2126450c6f717
mack0331 <ericjmackay@gmail.com>
2020-07-25 01:44:10 -0500

    Update Read.py
    Updating Read and Play Script

--- Read.py
+++ Read.py
@@ -47,3 +47,31 @@ try:
             current_book_id = int(10)
 
         start_time = time.time()
+
+        print("Ready to Read a Book")
+        id, book = reader.read()
+        if id == None:
+            status = True
+        print("Playing AudioBook: " + book)
+        print("ID is: " + str(id))
+        book = book.strip()
+
+        if current_book_id !=  id:
+            current_book_id = id
+            playaudio(book)
+#        else:
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+
+        if status == True:
+            if elapsed_time > 0.6:
+                if IsPaused == True:
+                    pygame.mixer.music.unpause()
+                    IsPaused = False
+                else:
+                    pygame.mixer.music.pause()
+                    IsPaused = True
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print("\nAll Done")
