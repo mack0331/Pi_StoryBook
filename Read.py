075bf3fd7da33aeedaa59f7694e080105f4912b9
mack0331 <ericjmackay@gmail.com>
2020-07-25 01:35:23 -0500

    Update Read.py

--- Read.py
+++ Read.py
@@ -2,21 +2,48 @@
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+import os
+import signal
+import time
 import pygame
+
+pygame.mixer.init()
+
+IsPaused = False
+
+def playaudio(audiofile):
+    global myprocess
+    global directory
+
+    status = pygame.mixer.music.get_busy()
+
+    if status == False:
+        print("No audio playing, start playing new audio")
+
+    else:
+        print("Audio alread playing, so quit current audio, then play")
+        pygame.mixer.music.stop()
+
+
+    pygame.mixer.music.load(directory + audiofile)
+    pygame.mixer.music.play()
+
+    time.sleep(3)
 
 reader = SimpleMFRC522()
 
-try:
-        id, text = reader.read()
-       # print(id)
-        print("Read File: " + text)
-finally:
-        GPIO.cleanup()
+directory = '/home/pi/ellie_pi/'
 
-	
-        pygame.mixer.init()
-        pygame.mixer.music.load(text.strip())
-        pygame.mixer.music.play()
-        print("Playing AudioBook: " + text)
-        while pygame.mixer.music.get_busy() == True:
-                continue
+print("Let's listen to some stories!")
+
+try:
+
+    while True:
+
+        status = pygame.mixer.music.get_busy()
+
+        if status == False:
+
+            current_book_id = int(10)
+
+        start_time = time.time()
